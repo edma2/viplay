76ec1e841c0d7f2b08be9f341746ce71552c998c
edma2 <edma2@san.rr.com>
2011-01-13 01:15:49 -0800

    client non curses

--- client.c
+++ client.c
@@ -1,15 +1,12 @@
 /* client.c - example client for player.c */
 #include "player.h"
-#include <ncurses.h>
 
 #define BUFSIZE         200
-#define PATHMAX         100
-#define VLINE_OFFSET    20
 
 int main(int argc, char *argv[]) {
         Player *mp;
-        char path[PATHMAX];
         char buf_out[BUFSIZE];
+        char buf_in[BUFSIZE];
         char c;
 
         mp = player_new();
@@ -25,49 +22,34 @@ int main(int argc, char *argv[]) {
                 return -1;
         }
 
-        /* Start ncurses */
-        initscr();
-        mvvline(0, VLINE_OFFSET, ACS_VLINE, 20);
-
         /* Input loop */
         while (1) {
-                mvprintw(0, VLINE_OFFSET, "> ");
-                c = getch();
+                fgets(buf_in, sizeof(buf_in), stdin);
+                c = buf_in[0];
                 if (c == 'q') {
                         player_cmd_quit(mp);
                         break;
                 } else if (c == 'p') {
                         player_query_percent(mp, buf_out, sizeof buf_out);
-                        mvprintw(0, VLINE_OFFSET, "--Percent: %s\n", buf_out);
+                        printf("--Percent: %s\n", buf_out);
                 } else if (c == 'a') {
                         player_query_title(mp, buf_out, sizeof buf_out);
-                        mvprintw(0, VLINE_OFFSET, "--Title: %s\n", buf_out);
+                        printf("--Title: %s\n", buf_out);
                 } else if (c == 't') {
                         player_cmd_pause(mp);
-                        printw("\n");
+                        printf("\n");
                 } else if (c == 'l') {
-                        mvprintw(0, VLINE_OFFSET, "\nEnter path of file: ");
-                        if (getnstr(path, sizeof path) == ERR) {
-                                fprintf(stderr, "error: getnstr() failed\n");
-                                break;
-                        }
-                        if (strlen(path) < sizeof path) {
-                                path[strlen(path)] = '\n';
-                        } else {
-                                fprintf(stderr, "error: path too long\n");
-                                break;
-                        }
-                        player_cmd_load(mp, path);
-                        mvprintw(0, VLINE_OFFSET, "Loaded file %s", path);
+                        printf("\nEnter path of file: ");
+                        if (fgets(buf_in, sizeof(buf_in), stdin) == NULL)
+                                continue;
+                        player_cmd_load(mp, buf_in);
+                        printf("Loaded file %s", buf_in);
                 }
-                /* Refresh ncurses window */
-                refresh();
         }
 
         /* Clean up*/
         player_die(mp);
         player_free(mp);
-        endwin();
 
         return 0;
 }
